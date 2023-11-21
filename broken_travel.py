da18b9548d7075a3f06909bf9d6514904549124a
jadnahlous <jadnahlouson@gmail.com>
2023-11-21 13:12:59 +0400

    the broken time traveler, 3rd commit

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,12 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year == int.input("Greetings! What is your year of origin? '))
+year = int(input("Greetings! What is your year of origin? "))  # I added ( instead of '.' and then replaced ' with "
 
-if year <= 1900
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
-    print ("That's totally the present!")
-elif:
-    print ("Far out, that's the future!!")
+if year <= 1900:  # added :
+    print("Woah, that's the past!")  # replaced ' with "
+elif 1900 <= year <= 2020:  # replaced && with putting year between 1900 and 2020 else we could have also put and
+    #  in addition, I added = so whenever it is 1900 or 2020 it would still print out the same values
+    print("That's totally the present!")
+else:  # replaced elif with else
+    print("Far out, that's the future!!")
